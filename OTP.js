6bf31107ac5abfbcb9fb684c4045daab851586fa
SatyaPrakash Sharma <sharmasatyaprakash169@gmail.com>
2024-09-01 19:58:16 +0530

    Generate OTPs

---
+++ OTP.js
@@ -0,0 +1,33 @@
+// const otp = Math.floor((Math.random() +1) * 2000) ;
+
+let otp2;
+
+document.getElementById("otpgen").innerHTML = `OTP Generator : "Click Generate"`;
+
+function newotp(){
+
+ otp2 =Math.floor((Math.random() +1) * 2000) ;
+
+document.getElementById("otpgen").innerHTML = `OTP Generator : "${otp2}"`;
+
+}
+
+
+
+
+
+function submit(){
+    let userConfirm = document.getElementById("userinp").value;
+    userConfirm = Number(userConfirm);
+    console.log(userConfirm)
+
+    if(otp2 == userConfirm){
+        alert(`${otp2} OTP Verified✅`);
+    }else{
+        alert(`Enter valid OTP`);
+    }
+}
+
+
+
+    
